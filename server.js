569a190176a1e31a2d34ed954eff1fa7f25b34b1
anelson0112 <71732184+anelson0112@users.noreply.github.com>
2020-12-03 13:43:53 -0600

    Added style

--- server.js
+++ server.js
@@ -1,19 +1,50 @@
+//require mongoose- translates built in to  Node.JS
 const mongoose = require('mongoose'); 
 
-//('mongodb://localhost/test', {useNewUrlParser: true, useUnifiedTopology: true});
+//connect to the cluster I created in Atlas
 const mongoDB = "mongodb+srv://anelson0112:10qpalzm7YGV@taskcluster.7xytg.mongodb.net/TaskListdb?retryWrites=true&w=majority";
 
+//accessing the connect method of mongoose
+//pass it the name of the DB cluster we have created
 mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
 
 const db = mongoose.connection;
 
-//but then we access the on and once methods of our connection property form our mongoose object 
+//turns on the connection
+
 db.on('error', console.error.bind(console, 'connection error:'));
+
+//connects to the database one time, runs the method and stops
+
 db.once('open', function(){
-    //your tutorial and new code go here. 
+    //test to see we're connected. 
     console.log("We're connected");
+//new Schema test
+    const taskSchema = new mongoose.Schema ({
+        name: String,
+        priority : String,
+
+    });
+
+
+const ToDo = mongoose.model('ToDo', taskSchema);
+
+const laundry = new ToDo ({name : "Laundry"});
+
+const dishes = new ToDo ({name : "dishes"});
+
+console.log(laundry.name);
+console.log(dishes.name);
+
+laundry.save(function(err,task){
+    if (err) return console.error(err);
+    console.log("saved");
+});
+dishes.save(function(err,task){
+    if (err) return console.error(err);
+    console.log("saved dishes");
 });
 
-
+});
 
 
