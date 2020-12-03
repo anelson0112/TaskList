f060f4a6bda33c53a2445334752f1e7f733f638c
anelson0112 <71732184+anelson0112@users.noreply.github.com>
2020-12-03 10:31:33 -0600

    connected DB

--- server.js
+++ server.js
@@ -1,23 +1,19 @@
-
+const mongoose = require('mongoose'); 
 
 //('mongodb://localhost/test', {useNewUrlParser: true, useUnifiedTopology: true});
-const db = mongoose.connect="mongodb+srv://anelson0112:10qpalzm7YGV@taskcluster.7xytg.mongodb.net/TaskListdb?retryWrites=true&w=majority"
+const mongoDB = "mongodb+srv://anelson0112:10qpalzm7YGV@taskcluster.7xytg.mongodb.net/TaskListdb?retryWrites=true&w=majority";
+
+mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
+
+const db = mongoose.connection;
+
+//but then we access the on and once methods of our connection property form our mongoose object 
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function(){
+    //your tutorial and new code go here. 
+    console.log("We're connected");
+});
 
 
 
 
-
-async function run() {
-    try {
-        await client.connect();
-        console.log("Connected correctly to server");
-
-    } catch (err) {
-        console.log(err.stack);
-    }
-    finally {
-        await client.close();
-    }
-}
-
-run().catch(console.dir);
